cb8262786db7c95a113248d87308a7019347ce61
Reapor-Yurnero <reapor.yurnero@gmail.com>
2019-07-16 17:31:02 +0800

    add more constants

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -19,18 +19,31 @@ export type UserStateProps = {
 }
 
 const INITIAL_STATE: UserStateProps = {
+    // gid for ve450
     gid: '5d2c22c662d30c1cc08aaa3f',
     // lyh
-    uid: '5d2c286762d30c1cc08aaa44',
-    keypair: {
-        publicKey: 'ROh0E1mJOFEEx/z3A2S7sKm3ZT88vKIdIJ/Bpj1h1GY=',
-        privateKey: '60qYjRlHzau5burcWwRJAwsujn5tCtiKt0j3qRkceWE='
-    },
+    // uid: '5d2c286762d30c1cc08aaa44',
+    // keypair: {
+    //     publicKey: 'ROh0E1mJOFEEx/z3A2S7sKm3ZT88vKIdIJ/Bpj1h1GY=',
+    //     privateKey: '60qYjRlHzau5burcWwRJAwsujn5tCtiKt0j3qRkceWE='
+    // },
     // cyg
     // uid: '5d2c2a5162d30c1cc08aaa46',
     // keypair: {
     //     publicKey: 'b//rwWJqdFW9el5FW0xnxKQmNRLAR0kuUe/2qQoG9nM=',
     //     privateKey: 'bHOLf11eK1tqcVOvXzo9O6I6dUk8NOecOyCKPXge+6Y='
+    // },
+    // hyc
+    uid: '5d2c2ab362d30c1cc08aaa47',
+    keypair: {
+        publicKey: '3nYmD5h1Mfewdc6KBRaeydFx5HK2qUfpI8eSxzpcgEE=',
+        privateKey: 'ib52sSPl0dhtX8h0K1XRm3iZHgoi9mNG8vTrtjJQneI='
+    },
+    // jsf
+    // uid: '5d2c2ab362d30c1cc08aaa47',
+    // keypair: {
+    //     publicKey: '/doOKD/6cwwA2RwdxgX2QPwMiHPdeBp1YSqckY9A9nc=',
+    //     privateKey: '0TJwC9hV+r9865yzy/HxdZEZquJuvspMzgSCnMNi/9k='
     // },
     target: null,
     matched: [],
